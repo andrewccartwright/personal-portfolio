cf509b863b7ee791763a088cc1f3bc630d2abfcd
andrewchatch <andrew.hatch96@gmail.com>
2024-01-13 20:22:17 -0700

    Update last name to Cartwright

--- src/components/about-me/sub-components/ContactMe.tsx
+++ src/components/about-me/sub-components/ContactMe.tsx
@@ -5,10 +5,10 @@ const ContactMe = (props: {}) => {
         <div id="contact-me-section" className="text-center">
             <h2 id="contact-me-title">Contact Me</h2>
 
-            <p> Resume: <a href="https://andrewchatch.github.io/interactive-resume/" target="_blank">Interactive Resume</a></p>
-            <p> Email: <a href="mailto: andrew.hatch96@gmail.com" data-rel="external" target="_blank">andrew.hatch96@gmail.com</a></p>
-            <p>LinkedIn: <a href="https://www.linkedin.com/in/andrew-hatch-8b13b0134" target="_blank">linkedin.com/in/andrew-hatch-8b13b0134</a></p>
-            <p>GitHub: <a href="https://github.com/andrewchatch" target="_blank">github.com/andrewchatch</a></p>
+            <p> Resume: <a href="https://andrewccartwright.github.io/interactive-resume/" target="_blank">Interactive Resume</a></p>
+            <p> Email: <a href="mailto: drew.c.cartwright@gmail.com" data-rel="external" target="_blank">drew.c.cartwright@gmail.com</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/andrew-cartwright-8b13b0134" target="_blank">linkedin.com/in/andrew-cartwright-8b13b0134</a></p>
+            <p>GitHub: <a href="https://github.com/andrewccartwright" target="_blank">github.com/andrewccartwright</a></p>
         </div>
     )
 }
